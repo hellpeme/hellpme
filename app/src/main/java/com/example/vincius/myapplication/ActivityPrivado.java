24542fd0411c9898be943eddd0a6425a6d1541fb
João Victor Duarte Viana <JoaoDeMaria@iMac-de-administrador-6.local>
2019-02-07 18:24:02 -0300

    Private Created

---
+++ app/src/main/java/com/example/vincius/myapplication/ActivityPrivado.java
@@ -0,0 +1,98 @@
+package com.example.vincius.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
+
+import java.util.List;
+
+public class ActivityPrivado extends AppCompatActivity {
+
+    private GroupAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privado);
+
+        RecyclerView rv = findViewById(R.id.recyclePrivate);
+
+        adapter = new GroupAdapter();
+
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        CollectionReference doc = FirebaseFirestore.getInstance().collection("users");
+        doc.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    return;
+                } else {
+                    List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot doc :
+                            docs) {
+                        User user = doc.toObject(User.class);
+                        Log.d("Teste", "onEvent: " + user.getUsername());
+                        adapter.add(new UsersItem(user));
+                    }
+                }
+
+            }
+        });
+
+
+    }
+
+
+    private class UsersItem extends Item<ViewHolder> {
+
+        private User user;
+
+        private UsersItem(User user){
+            this.user = user;
+        }
+
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+
+            TextView txtUser =  viewHolder.itemView.findViewById(R.id.textNameUserPesquisa);
+            ImageView imgUser = viewHolder.itemView.findViewById(R.id.ImageView);
+
+            txtUser.setText(user.getUsername());
+
+            Picasso.get()
+                    .load(user.getProfileUrl())
+                    .into(imgUser);
+
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_user;
+        }
+    }
+
+}
--- app/src/main/java/com/example/vincius/myapplication/Fragments/FragmentHome.java
+++ app/src/main/java/com/example/vincius/myapplication/Fragments/FragmentHome.java
@@ -1,5 +1,6 @@
 package com.example.vincius.myapplication.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,11 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vincius.myapplication.ActivityPrivado;
 import com.example.vincius.myapplication.R;
+import com.github.clans.fab.FloatingActionButton;
 
 public class FragmentHome extends Fragment {
 
     View view;
+
+
     public FragmentHome() {
         // Required empty public constructor
     }
@@ -35,7 +40,17 @@ public class FragmentHome extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        FloatingActionButton btnPrivate = view.findViewById(R.id.fabMonitoriaPrivada);
 
+        btnPrivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ActivityPrivado.class);
+
+                startActivity(intent);
+
+            }
+        });
 
 
     }
