d799d873884a26e1a5d00ddea2d1c08b46158c1d
viniciusmesquitac <viniciuscoelho.ti@gmail.com>
2019-03-15 20:46:10 -0300

    logica de pontos alterada

---
+++ app/src/main/java/com/example/vincius/myapplication/ActivitySubjectSelect.java
@@ -0,0 +1,13 @@
+package com.example.vincius.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySubjectSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_select);
+    }
+}
